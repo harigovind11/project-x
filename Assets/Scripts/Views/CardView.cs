c49887c3aa09dd68deb93357e54b2dff57cf6b1f
harigovind11 <harigovindsreejith007@gmail.com>
2025-08-14 00:43:26 +0530

    Adds match effect and smooth card removal animation

--- Assets/Scripts/Views/CardView.cs
+++ Assets/Scripts/Views/CardView.cs
@@ -10,17 +10,19 @@ public class CardView : MonoBehaviour
     [SerializeField] private Sprite _cardFace;
     [SerializeField] private Sprite _cardBack;
     [SerializeField] private SpriteRenderer _icon;
-    [SerializeField] private float _flipDuration = 0.5f; // Your flip animation duration
+    [SerializeField] private float _flipDuration = 0.5f;
+
+    [Header("Effects")]
+    [SerializeField] private GameObject _matchEffectPrefab;
+    [SerializeField] private float _matchAnimationDuration = 0.3f;
 
     //Private Animation State 
     private bool _isFlipping;
     private bool _faceUp;
     private SpriteRenderer _spriteRenderer;
 
-
     void Awake()
     {
-
         _spriteRenderer = GetComponent<SpriteRenderer>();
     }
 
@@ -34,15 +36,14 @@ public class CardView : MonoBehaviour
         _spriteRenderer.sprite = _cardBack;
         _icon.gameObject.SetActive(false);
         transform.localScale = Vector3.one;
+        gameObject.SetActive(true);
     }
 
     //Input
     private void OnMouseDown()
     {
-
         if (!_isFlipping && !IsCollected)
         {
-
             EventManager.RaiseCardFlipped(this);
         }
     }
@@ -56,26 +57,31 @@ public class CardView : MonoBehaviour
         }
     }
 
-
     public void SetAsMatched()
     {
         if (!IsCollected)
         {
             IsCollected = true;
-            // particle effects and scale animation here
-            gameObject.SetActive(false);
+
+            if (_matchEffectPrefab != null)
+            {
+                Instantiate(_matchEffectPrefab, transform.position, Quaternion.identity);
+            }
+
+            StartCoroutine(ScaleOutAndDeactivate());
         }
     }
 
-    // Animation Coroutine
+    // Animation Coroutines
     private IEnumerator FlipCard()
     {
         _isFlipping = true;
 
-        yield return FlipHalf(_flipDuration / 2, 1f, 0f);
+        Vector3 originalScale = transform.localScale;
+        yield return StartCoroutine(FlipHalf(_flipDuration / 2, originalScale.x, 0f));
         _spriteRenderer.sprite = _faceUp ? _cardBack : _cardFace;
         _icon.gameObject.SetActive(!_faceUp);
-        yield return FlipHalf(_flipDuration / 2, 0f, 1f);
+        yield return StartCoroutine(FlipHalf(_flipDuration / 2, 0f, originalScale.x));
 
         _faceUp = !_faceUp;
         _isFlipping = false;
@@ -83,13 +89,29 @@ public class CardView : MonoBehaviour
 
     private IEnumerator FlipHalf(float duration, float startScale, float endScale)
     {
+        Vector3 initialScale = transform.localScale;
         for (float time = 0; time < duration; time += Time.deltaTime)
         {
             var scale = Mathf.Lerp(startScale, endScale, time / duration);
-            transform.localScale = new Vector3(scale, 1, 1);
+            transform.localScale = new Vector3(scale, initialScale.y, initialScale.z);
             yield return null;
         }
 
-        transform.localScale = new Vector3(endScale, 1, 1);
+        transform.localScale = new Vector3(endScale, initialScale.y, initialScale.z);
+    }
+
+    private IEnumerator ScaleOutAndDeactivate()
+    {
+        Vector3 initialScale = transform.localScale;
+        Vector3 targetScale = Vector3.zero;
+
+        for (float time = 0; time < _matchAnimationDuration; time += Time.deltaTime)
+        {
+            transform.localScale = Vector3.Lerp(initialScale, targetScale, time / _matchAnimationDuration);
+            yield return null;
+        }
+
+        transform.localScale = targetScale;
+        gameObject.SetActive(false);
     }
 }
